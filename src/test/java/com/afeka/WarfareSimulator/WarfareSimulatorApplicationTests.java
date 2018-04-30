90470222928db993e486681fa016544b01353f54
Tomer Gliksman <glik44tomer@gmail.com>
2018-04-30 13:34:43 +0300

    create maven projects

---
+++ src/test/java/com/afeka/WarfareSimulator/WarfareSimulatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.afeka.WarfareSimulator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WarfareSimulatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
