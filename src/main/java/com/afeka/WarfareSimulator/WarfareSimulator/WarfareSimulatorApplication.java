da9978f7d871cd56bcb33fd59a48b59466e479f2
Tomer Gliksman <glik44tomer@gmail.com>
2018-04-30 13:15:05 +0300

    create maven project

---
+++ src/main/java/com/afeka/WarfareSimulator/WarfareSimulator/WarfareSimulatorApplication.java
@@ -0,0 +1,12 @@
+package com.afeka.WarfareSimulator.WarfareSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarfareSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarfareSimulatorApplication.class, args);
+	}
+}
